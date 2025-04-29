5f1f1165aa789ead40bbdbe434b650ac89401821
Vani1B <vani.boddu@gmail.com>
2025-04-29 23:24:41 +0530

    fix build issues and update project

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/api/test`)
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend Connected to Backend</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
